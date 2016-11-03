4d1ff75a552ee02af8e25dd28e3ebc932b792772
Nikita Veselov <qamelior@gmail.com>
2016-11-03 12:27:50 +0300

    follow-up camera added (switched off)

--- rope bridge/src/examples/example_shapes/bridge.h
+++ rope bridge/src/examples/example_shapes/bridge.h
@@ -5,6 +5,8 @@ namespace octet
 	{
 		plank,
 		cliff,
+		rope,
+		column
 	};
 	
 	class Bridge
@@ -25,6 +27,8 @@ namespace octet
 		vec3 cliff_size;
 		vec3 cliff_half_size;
 
+		vec3 rope_part_size;
+
 		mesh_instance *left_cliff_mesh;
 		vec3 left_cliff_location;
 		btRigidBody *left_cliff_rb;
@@ -44,8 +48,9 @@ namespace octet
 			plank_material = new material(vec4(0.54f, 0.27f, 0.074f, 1));
 			plank_half_size = plank_size / 2.f;
 
-			cliff_material = new material(vec4(0.586f, 0.55f, 0.598f, 1));
-			cliff_size = vec3(12, 20, 20);
+			//cliff_material = new material(vec4(0.586f, 0.55f, 0.598f, 1));
+			cliff_material = new material(vec4(0, 0.39f, 0, 1));
+			cliff_size = vec3(100, 100, 200);
 			cliff_half_size = cliff_size / 2.f;
 
 		}
@@ -53,7 +58,7 @@ namespace octet
 		void create_cliffs(int size)
 		{
 			mat.loadIdentity();
-			left_cliff_location = vec3(-20, cliff_half_size.y() + ground_level, 0);
+			left_cliff_location = vec3(-cliff_half_size.x()*3/2.f, cliff_half_size.y() + ground_level, 0);
 			mat.translate(left_cliff_location);
 			left_cliff_mesh = app_scene->add_shape(mat, new mesh_box(cliff_half_size), cliff_material,false);
 			left_cliff_rb = left_cliff_mesh->get_node()->get_rigid_body();
@@ -75,43 +80,111 @@ namespace octet
 			return mesh->get_node()->get_rigid_body();
 		}
 
-		void bind_bodies(btRigidBody *previous_rb, object_type previos_type, btRigidBody *current_rb, object_type current_type)
+		void bind_bodies(btRigidBody *previous_rb, object_type previos_type, btRigidBody *current_rb, object_type current_type, float break_limit)
 		{
-			float previous_object_width = 0.f,
-				previous_vertical_align = 0.f,
-				current_object_width = 0.f,
-				current_vertical_align = 0.f;
+			float pA_x = 0.f,
+				pA_y = 0.f,
+				pB_x = 0.f,
+				pB_y = 0.f;
 				
-			if (previos_type == plank)
+			switch (previos_type)
 			{
-				previous_object_width = plank_size.x();
-			}
-			else
-			{
-				previous_object_width = cliff_size.x();
-				previous_vertical_align = cliff_half_size.y() - plank_half_size.y();
-			}
-
-			if (current_type == plank)
-			{
-				current_object_width = plank_size.x();
-			}
-			else
-			{
-				current_object_width = cliff_size.x();
-				current_vertical_align = cliff_half_size.y() - plank_half_size.y();
+				case plank:
+					pA_x = (plank_size.x() + distance_between) / 2.f;
+					break;
+				case rope:
+					pA_x = rope_part_size.x();
+					pA_y = 0;
+					break;
+				case cliff:
+					pA_x = (cliff_size.x() + distance_between) / 2.f;
+					pA_y = cliff_half_size.y() - plank_half_size.y();
+				default:
+					break;
 			}
 
-			btVector3 pivotA((previous_object_width + distance_between) / 2.f, previous_vertical_align, 0.f);
-			btVector3 pivotB(-(current_object_width + distance_between) / 2.f, current_vertical_align, 0.f);
+			switch (current_type)
+			{
+			case octet::plank:
+				pB_x = -(plank_size.x() + distance_between) / 2.f;
+				break;
+			case octet::cliff:
+				pB_x = -(cliff_size.x() + distance_between) / 2.f;
+				pB_y = cliff_half_size.y() - plank_half_size.y();
+				break;
+			case octet::rope:
+				pB_x = -rope_part_size.x();
+				pB_y = 0;
+				break;
+			case octet::column:
+				break;
+			default:
+				break;
+			}
+
+			btVector3 pivotA(pA_x, pA_y, 0.f);
+			btVector3 pivotB(pB_x, pB_y, 0.f);
 			btVector3 axisA(0.f, 0.f, 1.f);
 			btVector3 axisB(0.f, 0.f, 1.f);
 
 			btHingeConstraint *hinge = new btHingeConstraint(*previous_rb, *current_rb, pivotA, pivotB, axisA, axisB);
-			hinge->setDbgDrawSize(btScalar(distance_between));
-			hinge->setLimit(-0.5f*SIMD_HALF_PI, 0.5f*SIMD_HALF_PI);
-			//hinge->setLimit
-			app_scene->add_hinge_constraint(hinge, true);
+			hinge->setDbgDrawSize(btScalar(0.f));
+			hinge->setLimit(-0.f*SIMD_HALF_PI, 0.f*SIMD_HALF_PI,1.f,0.3f,0.f);
+			hinge->setBreakingImpulseThreshold(break_limit);
+			//hinge->set
+			app_scene->add_hinge_constraint(hinge,false);
+		}
+
+		void create_columns()
+		{
+			vec3 start = vec3(left_cliff_location.x() + cliff_half_size.x() - 2, left_cliff_location.y() + cliff_half_size.y(), left_cliff_location.z());
+			vec3 end = vec3(right_cliff_location.x() - cliff_half_size.x() + 2, right_cliff_location.y() + cliff_half_size.y(), right_cliff_location.z());
+
+			vec3 column_size = vec3(0.5f, 2.f, 0.5f);
+			mat.loadIdentity();
+			mat.translate(start + vec3(0, column_size.y()/2.f, 0));
+			btRigidBody *left_column = app_scene->add_shape(mat, new mesh_box(column_size), new material(vec4(1, 1, 1, 1)), false)->get_node()->get_rigid_body();
+
+			mat.loadIdentity();
+			mat.translate(end + vec3(0, column_size.y()/2.f, 0));
+			btRigidBody *right_column = app_scene->add_shape(mat, new mesh_box(column_size), new material(vec4(1, 1, 1, 1)), false)->get_node()->get_rigid_body();
+
+			create_rope(left_column,right_column, start,end, column_size);
+		}
+
+		btRigidBody *create_rope_part(vec3 location, material *part_material)
+		{
+			mat.loadIdentity();
+			mat.translate(location);
+			mesh_instance *mesh = app_scene->add_shape(mat, new mesh_box(vec3(rope_part_size)), part_material, true);
+			return mesh->get_node()->get_rigid_body();
+		}
+
+		void create_rope(btRigidBody *left_column, btRigidBody *right_column, vec3 start, vec3 end, vec3 column_size)
+		{
+			int parts_number = 80;
+
+			rope_part_size = vec3((end - start).x() / 2.f/parts_number, 0.2f, 0.2f);
+			material *rope_material = new material(vec4(0.9, 0.9, 0.9, 1));
+			//cliff-bridge hinge
+			vec3 from = start + vec3(0, 2 * column_size.y() + rope_part_size.y(), 0);
+			vec3 to = end + vec3(0, 2 * column_size.y() + rope_part_size.y(), 0);
+
+			vec3 location = vec3(from.x() + rope_part_size.x(), from.y(), from.z());
+			btRigidBody *current, *previous = create_rope_part(location, rope_material);
+
+			bind_bodies(left_column, column, previous, rope,10000.f);
+			
+			for (int i = 1; i < parts_number; i++)
+			{
+				location += 2 * rope_part_size.x();
+				current = create_rope_part(location, rope_material);
+
+				bind_bodies(previous, rope, current, rope, 10000.f);
+				previous = current;
+
+			}
+			bind_bodies(previous, rope, right_column, column, 10000.f);
 		}
 
 	public:
@@ -133,17 +206,17 @@ namespace octet
 			vec3 location = vec3(left_cliff_location.x() + distance_between + plank_size.x() + cliff_half_size.x(),
 				left_cliff_location.y() + cliff_half_size.y() - plank_half_size.y(), left_cliff_location.z());
 			
-			
+			float break_limit = 20.f;
 
 			btRigidBody *previous_body = create_plank(location);
-			bind_bodies(left_cliff_rb, cliff, previous_body, plank);
+			bind_bodies(left_cliff_rb, cliff, previous_body, plank, break_limit);
 
 			btRigidBody *current_body;
 			for (int i = 1; i < planks_number; i++)
 			{
 				location = vec3(location.x() + distance_between + plank_size.x(), location.y(), location.z());
 				current_body = create_plank(location);
-				bind_bodies(previous_body, plank, current_body, plank);
+				bind_bodies(previous_body, plank, current_body, plank, break_limit);
 				previous_body = current_body;
 			}
 
@@ -154,16 +227,31 @@ namespace octet
 			////frameA.setOrigin(btVector3(location.x() - cliff_size.x(), right_cliff_location.y() - cliff_size.y(),
 			////	right_cliff_location.z() - cliff_size.z()));
 			//frameA.setOrigin(btVector3(location.x(), location.y(), location.z()));
-
 			//btHingeConstraint *h = new btHingeConstraint(*previous_body, frameA);
 			//h->setDbgDrawSize(btScalar(distance_between));
 			//h->setLimit(-0.5f*SIMD_HALF_PI, 0.5f*SIMD_HALF_PI);
 			//app_scene->add_hinge_constraint(h, true);
 
 			//bridge-bank hinge
-			bind_bodies(previous_body, plank, right_cliff_rb, cliff);
+			bind_bodies(previous_body, plank, right_cliff_rb, cliff, break_limit);
+
+			//create_columns();
 		}
 
+		vec3 get_camera_starting_point()
+		{
+			//return (right_cliff_location);
+			return (vec3((right_cliff_location.x() + left_cliff_location.x()) /2.f, right_cliff_location.y() + cliff_half_size.y(), right_cliff_location.z() - 0));
+		}
 
+		vec3 get_start()
+		{
+			return vec3(left_cliff_location.x(), left_cliff_location.y() + cliff_half_size.y(), left_cliff_location.z());
+		}
+
+		vec3 get_end()
+		{
+			return vec3(right_cliff_location.x(), right_cliff_location.y() + cliff_half_size.y(), right_cliff_location.z());
+		}
 	};
 }
--- rope bridge/src/examples/example_shapes/rope bridge.h
+++ rope bridge/src/examples/example_shapes/rope bridge.h
@@ -12,8 +12,18 @@ namespace octet {
   class example_shapes : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
+	//bridge constructor
 	Bridge *bridge;
+
+	mouse_look mouse_look_helper;
+	helper_fps_controller fps_helper;
+
+	ref<scene_node> player_node;
+
+	ref<camera_instance> the_camera;
+
 	float number = 0;
+	btRigidBody *ball;
   public:
     example_shapes(int argc, char **argv) : app(argc, argv) {
     }
@@ -22,65 +32,107 @@ namespace octet {
     }
 
     /// this is called once OpenGL is initialized
-    void app_init() {
-      app_scene =  new visual_scene();
-      app_scene->create_default_camera_and_lights();
-	  //app_scene->get_camera_instance(0)->
-      app_scene->get_camera_instance(0)->get_node()->translate(vec3(10, 25, 1));
+	void app_init() {
+		mouse_look_helper.init(this, 100.f/360.f, false);
+		fps_helper.init(this, vec3(10.f, 4.f, 0.f));
+		
+		app_scene = new visual_scene();
+		app_scene->create_default_camera_and_lights();
 
-	  app_scene->get_camera_instance(0)->get_node()->rotate(-40.f, vec3(1, 0, 0));
-	  //app_scene->get_camera_instance(0)->get_node()->rotate(-45.f, vec3(1, 0, 0));
-	  //app_scene->get_camera_instance(0)->get_node()->translate(vec3(0.f, 45.f, 0.f));
-	 
-	  //app_scene->get_camera_instance(0)->get_node()->rotate(-60.f, vec3(0.f, 1.f, 0.f));
-	  //app_scene->get_camera_instance(0)->get_node()->translate(vec3(-55.f, 0.f, 0.f));
+		the_camera = app_scene->get_camera_instance(0);
+		the_camera->get_node()->translate(vec3(0, 32, 0));
+		//how far can you see
+		the_camera->set_far_plane(1000);
 
-	  //app_scene->get_camera_instance(0)->get_node()->rotate(-45.f, vec3(0, 0, 1));
-	  //app_scene->get_camera_instance(0)->get_node()->translate(vec3(0.f, 0.f, 0.f));
+		app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 80, 60));
 
-	  //app_scene->get_camera_instance(0)->get_node()->translate(vec3(0.f, 45.f, 0.f));
-	 // app_scene
-      material *red = new material(vec4(1, 0, 0, 1));
-      material *water = new material(vec4(0, 0, 1, 1));
-      
-      mat4t mat;
-      //mat.translate(-10, -5, 0);
-      //app_scene->add_shape(mat, new mesh_box(vec3(2, 3, 2)), blue, true);
-	  //mat.loadIdentity();
-	  //mat.translate(10, -5, 0);
-	  //mesh_instance *right_cliff_mesh = app_scene->add_shape(mat, new mesh_box(vec3(2, 3, 2)), blue, true);
-	  //btRigidBody *right_cliff_rb = right_cliff_mesh->get_node()->get_rigid_body();	  
-	  //mat.loadIdentity();
-	  //mat.translate( 3, 6, 0);
-	  //app_scene->add_shape(mat, new mesh_cylinder(zcylinder(vec3(0, 0, 0), 2, 4)), blue, true);
-	  // ground
+		// app_scene
+		material *red = new material(vec4(1, 0, 0, 1));
+		material *water = new material(vec4(0, 0, 1, 1));
 
-	  mat.loadIdentity();
-	  mat.translate(0, 0, 0);
-	  app_scene->add_shape(mat, new mesh_box(vec3(100, 1, 200)), water, false);
-	  bridge = new Bridge(app_scene, mat, 1);
-	  bridge->create_bridge(20);
+		mat4t mat;
 
-	  mat.loadIdentity();
-	  mat.translate(-10, 30, 0);
-	  app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2),2), red, true,10);
-	  mat.loadIdentity();
-    }
+		// ground
+		mat.loadIdentity();
+		mat.translate(0, 0, 0);
+		app_scene->add_shape(mat, new mesh_box(vec3(400, 1, 200)), water, false);
+
+		//bridge
+		bridge = new Bridge(app_scene, mat, 1);
+		bridge->create_bridge(30);
+
+		//ball
+		mat.loadIdentity();
+		mat.translate(bridge->get_camera_starting_point() + vec3(0, 10,0));
+		//mesh_instance *ball_mesh = app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2), 2), red, true);
+		
+		//float player_height = 40.f;//1.83f;
+		float player_radius = 2.f;
+		float player_mass = 2.f;
+
+
+		//mesh_instance *mi = app_scene->add_shape(
+		//	mat,
+		//	new mesh_sphere(vec3(0), player_radius),
+		//	red,
+		//	true, player_mass
+		//);
+		////ball = ball_mesh->get_node()->get_rigid_body();
+		//
+		//player_node = mi->get_node();
+	}
+
+	void move_ball()
+	{
+		float ball_speed = 5.0f,
+			jump_force = 20.f;
+		if (is_key_down(key_up))
+		{
+			ball->applyForce(btVector3(ball_speed, 0.f, 0.f), btVector3(0, 0, 0));
+		}
+		else if (is_key_down(key_down))
+		{
+			ball->applyForce(btVector3(-ball_speed, 0.f, 0.f), btVector3(0, 0, 0));
+		}
+		else if (is_key_down(key_left))
+		{
+			ball->applyForce(btVector3(0.f, 0.f, -ball_speed), btVector3(0, 0, 0));
+		}
+		else if (is_key_down(key_right))
+		{
+			ball->applyForce(btVector3(0.f, 0.f, ball_speed), btVector3(0, 0, 0));
+		}
+		else if (is_key_down(key_space))
+		{
+			ball->applyForce(btVector3(0.f, jump_force, 0.f), btVector3(0, 0, 0));
+		}
+		
+
+		
+	}
 
     /// this is called to draw the world
     void draw_world(int x, int y, int w, int h) {
       int vx = 0, vy = 0;
       get_viewport_size(vx, vy);
-      app_scene->begin_render(vx, vy, vec4(0.53f, 0.8f, 0.976f, 0));
+	  app_scene->begin_render(vx, vy, vec4(0.53f, 0.8f, 0.976f, 0));
+	  
+	  //scene_node *camera_node = the_camera->get_node();
+	 // mat4t &camera_to_world = camera_node->access_nodeToParent();
+	  //mouse_look_helper.update(camera_to_world);
 
-      // update matrices. assume 30 fps.
+	  //fps_helper.update(player_node, camera_node);
+
+	  // update matrices. assume 30 fps.
       app_scene->update(1.0f/30);
+
+	  //move_ball();
 
       // draw the scene
       app_scene->render((float)vx / vy);
 	 // number += 0.5f;
 	  //app_scene->get_camera_instance(0)->get_node()->rotate(-1.f, vec3(1, 0, 0));
-	  //app_scene->get_camera_instance(0)->get_node()->translate(vec3(0.f, 0.f, -0.05f));
+	  //app_scene->get_camera_instance(0)->get_node()->translate(vec3(0.f, 0.f, 0.1f));
 	  //std::cout << number << std::endl;
     }
   };
