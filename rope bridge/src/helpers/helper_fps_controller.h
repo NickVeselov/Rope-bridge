08704c8711202d2cc59fc2d9d22c65af05cc6d1f
Nikita Veselov <qamelior@gmail.com>
2016-11-03 12:32:11 +0300

    .

--- rope bridge/src/helpers/helper_fps_controller.h
+++ rope bridge/src/helpers/helper_fps_controller.h
@@ -10,12 +10,16 @@ namespace octet { namespace helpers {
   /// Class for rotating the camera with the mouse.
   class helper_fps_controller {
     app *the_app;
+	vec3 camera_translation;
+	float velocity;
   public:
     helper_fps_controller() {
     }
 
-    void init(app *the_app) {
+    void init(app *the_app, vec3 Camera_translation = vec3(0, 1.25f, 0),float Velocity = 10.f) {
       this->the_app = the_app;
+	  camera_translation = Camera_translation;
+	  velocity = Velocity;
     }
 
     /// called every frame to update the player physics
@@ -25,31 +29,31 @@ namespace octet { namespace helpers {
         player_node->set_angular_factor(0);
         //player_node->set_angular_velocity(vec3(0, 0, 0));
         //player_node->set_rotation(mat4t());
-        player_node->clamp_linear_velocity(10);
+        player_node->clamp_linear_velocity(velocity);
 
         float friction = 0.0f;
         if (the_app->is_key_down('A')) {
           player_node->activate();
-          player_node->apply_central_force(camera_node->get_x() * (-1000.0f));
+          player_node->apply_central_force(camera_node->get_x() * (-velocity));
         } else if (the_app->is_key_down('D')) {
           player_node->activate();
-          player_node->apply_central_force(camera_node->get_x() * (+1000.0f));
+          player_node->apply_central_force(camera_node->get_x() * (+velocity));
         } else if (the_app->is_key_down('W')) {
           player_node->activate();
-          player_node->apply_central_force(camera_node->get_z() * (-1000.0f));
+          player_node->apply_central_force(camera_node->get_z() * (-velocity));
         } else if (the_app->is_key_down('S')) {
           player_node->activate();
-          player_node->apply_central_force(camera_node->get_z() * (+1000.0f));
+          player_node->apply_central_force(camera_node->get_z() * (+velocity));
         } else {
           friction = 1.0f;
         }
         if (the_app->is_key_going_down(' ')) {
-          player_node->apply_central_force(camera_node->get_y() * (+10000.0f));
+          player_node->apply_central_force(camera_node->get_y() * (+10*velocity));
         }
         player_node->set_friction(friction);
 
         mat4t &camera_to_world = camera_node->access_nodeToParent();
-        camera_to_world.w() = (player_node->get_position() + vec3(0, 1.25f , 0) ).xyz1();
+        camera_to_world.w() = (player_node->get_position() + camera_translation ).xyz1();
       #endif
     }
   };
